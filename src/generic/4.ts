430cd12ae4950b4634f5bc3540cd1915e9abee8f
Zen6ka <131902026+Zen6ka@users.noreply.github.com>
2024-02-27 14:13:03 +0200

    1

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,32 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+// class Component {
+//   constructor (public props:T) {
 
-  }
+//   }
+// }
+
+// class Page extends Component {
+//   pageInfo () {
+//     console.log(this.props.title);
+//   }
+// }
+
+// export {};
+
+interface ComponentProps {
+	title: string;
 }
 
-class Page extends Component {
-  pageInfo () {
-    console.log(this.props.title);
-  }
+class Component<T> {
+	constructor(public props: T) {}
+}
+
+class Page extends Component<ComponentProps> {
+	pageInfo(): void {
+		console.log(this.props.title);
+	}
 }
 
 export {};
--- src/generic/8.ts
+++ src/generic/8.ts
@@ -3,22 +3,42 @@
   Ви хочете створити новий тип Params, який включає всі поля з Form, крім errors.
 */
 
+// type Errors = {
+//   email?: string[];
+//   firstName?: string[];
+//   lastName?: string[];
+//   phone?: string[];
+// };
+
+// type Form = {
+//   email: string | null;
+//   firstName: string | null;
+//   lastName: string | null;
+//   phone: string | null;
+//   errors: Errors;
+// };
+
+// // Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
+// type Params = Form;
+
+// export {};
+
 type Errors = {
-  email?: string[];
-  firstName?: string[];
-  lastName?: string[];
-  phone?: string[];
+	email?: string[];
+	firstName?: string[];
+	lastName?: string[];
+	phone?: string[];
 };
 
 type Form = {
-  email: string | null;
-  firstName: string | null;
-  lastName: string | null;
-  phone: string | null;
-  errors: Errors;
+	email: string | null;
+	firstName: string | null;
+	lastName: string | null;
+	phone: string | null;
+	errors: Errors;
 };
 
 // Реалізуйте Params так, щоб унеможливити поле 'errors' з типу Form
-type Params = Form;
+type Params = Omit<Form, "errors">;
 
 export {};
